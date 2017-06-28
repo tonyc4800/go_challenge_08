2c810071e525f2419c39477a35fdc2b287bc67ba
Jack Burdick <jackbburdick@gmail.com>
2017-06-28 09:53:45 -0400

    create preliminary test file and read contents

--- sudoku/sudoku.go
+++ sudoku/sudoku.go
@@ -1,1 +1,13 @@
 package sudoku
+
+import "fmt"
+import "io/ioutil"
+
+func solveSudoku(path string) (string, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf("%v\n", string(data))
+	return "n", nil
+}
--- sudoku/sudoku_test.go
+++ sudoku/sudoku_test.go
@@ -1,1 +1,26 @@
 package sudoku
+
+import (
+	"testing"
+)
+
+func Test_solveSudoku(t *testing.T) {
+	var cases = []struct {
+		fpath string
+		sS    string
+	}{
+		{
+			"./input/puzzle_01.txt",
+			"yes",
+		},
+	}
+	for _, c := range cases {
+		sS, err := solveSudoku(c.fpath)
+		if err != nil {
+			t.Errorf("Error retriving solved Sudoku: %v", err)
+		}
+		if sS != c.sS {
+			t.Errorf("solved Sudoku does not match target\n")
+		}
+	}
+}
