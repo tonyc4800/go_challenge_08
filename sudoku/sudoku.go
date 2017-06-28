574693c8344304d9737aedde02eb7a3c6e5705ac
Jack Burdick <jackbburdick@gmail.com>
2017-06-28 10:36:05 -0400

    move gobal information w/in func, create rowUnits

--- sudoku/sudoku.go
+++ sudoku/sudoku.go
@@ -3,13 +3,7 @@ package sudoku
 import "fmt"
 import "io/ioutil"
 
-// global board information
-// `:=` only works within function bodies
-// TODO: eventually, this should be moved within an init function
-var rows = "ABCDEFGHI"
-var cols = "123456789"
-
-func createBoardIndex(A string, N string) []string {
+func crossIndex(A string, N string) []string {
 	var k string
 	var indexes []string
 	for _, a := range A {
@@ -28,9 +22,34 @@ func solveSudoku(path string) (string, error) {
 	}
 	fmt.Printf("%v\n", string(data))
 
-	ind := createBoardIndex(rows, cols)
-	// convert to grid
+	// Global board information.  The Sudoku board is assumed to be a standard
+	// 9x9 (A-I)x(1-9) grid -- where the first index (upper left) would be `A1`
+	// and the last index (lower right) would be `I9`.
+	rows := "ABCDEFGHI"
+	cols := "123456789"
+
+	ind := crossIndex(rows, cols)
 	fmt.Println(ind)
+
+	// Create units. units in this context will be groups of grid indexes that
+	// can only contain one instance of a number 1-9.  In Sudoku, a unit will be
+	// considered a `rowUnits`(horizontal), a `colUnits`(vertical), and a
+	// `blockUnits`(3x3 grid).
+
+	// Create `rowUnits`.
+	var rowUnits [][]string
+	for _, r := range rows {
+		cUnit := crossIndex(string(r), cols)
+		rowUnits = append(rowUnits, cUnit)
+	}
+	fmt.Println(rowUnits)
+
+	// Create `colUnits`.
+
+	// Create `blockUnits`.
+
+	// Convert to grid
+
 	//
 	return "n", nil
 }
