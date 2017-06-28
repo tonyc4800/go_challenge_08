4b23367a382459d38b5a7e8176d7167a6c19ac2d
Jack Burdick <jackbburdick@gmail.com>
2017-06-28 12:08:30 -0400

    move unitSlice creation to seperate function

--- sudoku/sudoku.go
+++ sudoku/sudoku.go
@@ -3,16 +3,56 @@ package sudoku
 import "fmt"
 import "io/ioutil"
 
+// crossIndex 'crosses'|'zips' two strings such that the two individual values
+// from each string join together to create a new value.  For example, if string
+// one is "ABC" and string two is "123", the resulting return value will be
+// ["A1","A2","A3","B1","B2","B3","C1","C2","C3"].
 func crossIndex(A string, N string) []string {
-	var k string
-	var indexes []string
+	var ks []string
 	for _, a := range A {
 		for _, n := range N {
-			k = string(a) + string(n)
-			indexes = append(indexes, k)
+			ks = append(ks, (string(a) + string(n)))
 		}
 	}
-	return indexes
+	return ks
+}
+
+// createUnitsSlice creates all units. units in this context will be groups of
+// grid indexes that can only contain one instance of a number 1-9.  In Sudoku,
+// a unit will be considered a `rowUnits`(horizontal), a `colUnits`(vertical),
+// and a `blockUnits`(3x3 grid).
+func createUnitsSlice(rows string, cols string) [][]string {
+	var unitsSlice [][]string
+
+	// Create `rowUnits` and append to slice of all units.
+	// i.e. > [[A1 A2 A3 A4 A5 A6 A7 A8 A9]...]
+	var rowUnits [][]string
+	for _, r := range rows {
+		rowUnits = append(rowUnits, crossIndex(string(r), cols))
+	}
+	unitsSlice = append(unitsSlice, rowUnits...)
+
+	// Create `colUnits` and append to slice of all units.
+	// i.e. > [[A1 B1 C1 D1 E1 F1 G1 H1 I1]...]
+	var colUnits [][]string
+	for _, c := range cols {
+		colUnits = append(colUnits, crossIndex(rows, string(c)))
+	}
+	unitsSlice = append(unitsSlice, colUnits...)
+
+	// Create `blockUnits` and append to slice of all units.
+	// i.e. > [[A1 A2 A3 B1 B2 B3 C1 C2 C3]...]
+	var blockUnits [][]string
+	rowGroup := [3]string{"ABC", "DEF", "GHI"}
+	colGroup := [3]string{"123", "456", "789"}
+	for _, ri := range rowGroup {
+		for _, ci := range colGroup {
+			blockUnits = append(blockUnits, crossIndex(ri, ci))
+		}
+	}
+	unitsSlice = append(unitsSlice, blockUnits...)
+
+	return unitsSlice
 }
 
 func solveSudoku(path string) (string, error) {
@@ -31,40 +71,9 @@ func solveSudoku(path string) (string, error) {
 	ind := crossIndex(rows, cols)
 	fmt.Println(ind)
 
-	// Create units. units in this context will be groups of grid indexes that
-	// can only contain one instance of a number 1-9.  In Sudoku, a unit will be
-	// considered a `rowUnits`(horizontal), a `colUnits`(vertical), and a
-	// `blockUnits`(3x3 grid).
-
-	var unitsList [][]string
-	// Create `rowUnits`.
-	// rowUnits will look similar to > [[A1 A2 A3 A4 A5 A6 A7 A8 A9]...]
-	var rowUnits [][]string
-	for _, r := range rows {
-		rowUnits = append(rowUnits, crossIndex(string(r), cols))
-	}
-	unitsList = append(unitsList, rowUnits...)
-
-	// Create `colUnits`.
-	// colUnits will look similar to > [[A1 B1 C1 D1 E1 F1 G1 H1 I1]...]
-	var colUnits [][]string
-	for _, c := range cols {
-		colUnits = append(colUnits, crossIndex(rows, string(c)))
-	}
-	unitsList = append(unitsList, colUnits...)
-
-	// Create `blockUnits`.
-	// blockUnits will look similar to > [[A1 A2 A3 B1 B2 B3 C1 C2 C3]...]
-	var blockUnits [][]string
-	rowGroup := [3]string{"ABC", "DEF", "GHI"}
-	colGroup := [3]string{"123", "456", "789"}
-	for _, ri := range rowGroup {
-		for _, ci := range colGroup {
-			blockUnits = append(blockUnits, crossIndex(ri, ci))
-		}
-	}
-	unitsList = append(unitsList, blockUnits...)
-	fmt.Println(unitsList)
+	// Create slice of all units in the Sudoku board.
+	unitsAll := createUnitsSlice(rows, cols)
+	fmt.Println(unitsAll)
 
 	// Convert to grid
 
